135c3c89535bc972d26ebd37ecae6da329d1b488
testusuke <testusuke@gmail.com>
2024-03-26 23:33:45 +0900

    feat: add sample pages

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,12 +1,9 @@
 import {Stack, Typography} from "@mui/material";
-import Userinfo from "@/components/Userinfo";
 
-export default function Home() {
+export default function Form() {
     return (
         <Stack height="100lvh" justifyContent="center" alignItems="center" gap="32px">
-            <Typography variant="h3">Home</Typography>
-
-            <Userinfo />
+            <Typography variant="h3">/form route</Typography>
         </Stack>
     );
 }
