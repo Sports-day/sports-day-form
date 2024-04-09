afe5e155043101d1e6cb8f159bb2e67bb13cb6af
Takkun0310 <takkunsan0310@gmail.com>
2024-04-09 19:18:11 +0900

    Cardをコンポーネント化した

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,37 +1,13 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import {Card, CardContent} from "@mui/material";
+import ListCard from "@/components/ListCard";
 
 
 export default function Form() {
     return (
         <>
             <Header />
-            <Card
-                sx={{
-                    display: 'flex',
-                    borderRadius: '20px',
-                    justifyContent: 'center',
-                    alignItems: 'flex-start',
-                    padding: 2,
-                    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.05)',
-                    maxWidth: '75%',
-                    margin: '0 auto',
-                    height: '50vh',
-                    marginTop: 4,
-                    '@media (min-width:600px)': {
-                        maxWidth: '75%',
-                        marginTop: 8
-                    },
-                    color: 's-lightest.main',
-                    backgroundColor: 's-lightest.main'
-                }}
-            >
-                <CardContent>
-
-                </CardContent>
-
-            </Card>
+            <ListCard />
             <Footer/>
         </>
     );
