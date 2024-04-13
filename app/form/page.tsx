0aad2445374ce33fd601f9ab0b7eaf61b0aac57e
Takkun0310 <takkunsan0310@gmail.com>
2024-04-13 11:22:17 +0900

    バーとメインコンテンツの干渉修正

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,14 +1,14 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import ListCard from "@/components/ListCard";
-
+import SportList from "@/components/SportList";
+import {Box} from "@mui/material";
 
 export default function Form() {
     return (
-        <>
+        <Box sx={{ paddingTop: '50px', paddingBottom: '80px'}}>
             <Header />
-            <ListCard />
+            <SportList />
             <Footer/>
-        </>
+        </Box>
     );
 }
