8e5f2631284059b873763b2a03702a3103fce712
Takkun0310 <takkunsan0310@gmail.com>
2024-04-18 16:32:52 +0900

    Add removing team alert function

--- components/DeleteTeam.tsx
+++ components/DeleteTeam.tsx
@@ -3,34 +3,69 @@ import Button from "@mui/material/Button";
 import React from "react";
 import { teamFactory } from "@/src/models/TeamModel";
 import { useRouter } from "next/navigation";
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 
 export type DeleteTeamProps = {
-    teamId: number
+    teamId: number;
 }
 
 export default function DeleteTeam(props: DeleteTeamProps) {
-    const router = useRouter()
+    const router = useRouter();
+    const [open, setOpen] = React.useState(false);
 
-    const onClickHandle = async () =>{
+    const handleOpen = () => {
+        setOpen(true);
+    };
 
-        await teamFactory().delete(props.teamId)
-        router.refresh()
-    }
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleDelete = async () => {
+        await teamFactory().delete(props.teamId);
+        router.refresh();
+        handleClose(); // ダイアログを閉じる
+    };
+
     return (
-
-        <Button
-            onClick={onClickHandle}
-            variant="outlined"
-            size="small"
-            fullWidth
-            color="warning"
-            style={{ border: '1px solid' }}
-            sx={{
-                borderRadius: "9px",
-                py: 0.3
-            }}
-        >
-            このチームを消す
-        </Button>
-    )
+        <React.Fragment>
+            <Button
+                onClick={handleOpen}
+                variant="outlined"
+                size="small"
+                fullWidth
+                color="warning"
+                style={{border: '1px solid'}}
+                sx={{
+                    borderRadius: "9px",
+                    py: 0.3
+                }}
+            >
+                このチームを消す
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title" color='s-darkest.main' fontWeight='bold'>
+                    {"チームを削除しますか?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description"　color='black'>
+                        この操作は元に戻せません。チームを削除してもよろしいですか？
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        キャンセル
+                    </Button>
+                    <Button onClick={handleDelete} color="error" autoFocus>
+                        削除
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
 }
