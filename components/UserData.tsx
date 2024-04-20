d9de67d226471c512ef8fb5debc609eca4ed8b08
Takkun0310 <takkunsan0310@gmail.com>
2024-04-21 02:42:29 +0900

    未登録のtagを追加

--- components/UserData.tsx
+++ components/UserData.tsx
@@ -1,9 +1,9 @@
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
-import {User} from "@/src/models/UserModel";
-import {teamFactory} from "@/src/models/TeamModel";
-import {teamTagFactory} from "@/src/models/TeamTagModel";
-import {Fragment} from "react";
+import { User } from "@/src/models/UserModel";
+import { teamFactory } from "@/src/models/TeamModel";
+import { teamTagFactory } from "@/src/models/TeamTagModel";
+import { Fragment } from "react";
 
 export type UserDataProps = {
     user: User;
@@ -23,28 +23,44 @@ export default async function UserData(props: UserDataProps) {
     const teamInfoList: TeamInfo[] = filteredTeams.map(team => {
         return {
             teamName: team.name,
-            tagName: (teamTags.find(tag => tag.id === team.teamTagId) || {name: 'No tag'}).name
+            tagName: (teamTags.find(tag => tag.id === team.teamTagId) || { name: '未登録' }).name
         }
     })
 
-    // タグ名に基づいてソート
     teamInfoList.sort((a, b) => {
         if (a.tagName.includes("晴天時") && !b.tagName.includes("晴天時")) return -1;
         if (!a.tagName.includes("晴天時") && b.tagName.includes("晴天時")) return 1;
         return 0;
     })
 
+    // チーム情報がない場合も「未登録」を表示
+    if (teamInfoList.length === 0) {
+        teamInfoList.push({ teamName: '未登録', tagName: '未登録' });
+    }
+
+    // チーム情報が1つの場合も2つ目のチーム情報を追加しておく
+    if (teamInfoList.length === 1) {
+        teamInfoList.push({ teamName: '未登録', tagName: '未登録' });
+    }
 
     const teamInfoComponents = teamInfoList.map((info, index) => (
         <Fragment key={index}>
             <TableCell
-                sx={{fontWeight: 'bold', color: '#2F3C8C', border: 0}}
+                sx={{
+                    fontWeight: 'bold',
+                    color: info.tagName === '未登録' ? 'red' : '#2F3C8C',
+                    border: 0
+                }}
                 align="left"
             >
                 {info.tagName}
             </TableCell>
             <TableCell
-                sx={{fontWeight: 'bold', color: '#2F3C8C', border: 0}}
+                sx={{
+                    fontWeight: 'bold',
+                    color: info.teamName === '未登録' ? 'red' : '#2F3C8C',
+                    border: 0
+                }}
                 align="left"
             >
                 {info.teamName}
@@ -55,13 +71,13 @@ export default async function UserData(props: UserDataProps) {
     const alertOutOfRange = (
         <Fragment>
             <TableCell
-                sx={{fontWeight: 'bold', color: 'red', border: 0}}
+                sx={{ fontWeight: 'bold', color: 'red', border: 0 }}
                 align="left"
             >
-                ３つ以上のチームに所属しています。<br/>
+                ３つ以上のチームに所属しています。<br />
                 {teamInfoList.map((info, index) => (
                     <Fragment key={index}>
-                        {info.tagName} チーム{info.teamName}<br/>
+                        {info.tagName} チーム{info.teamName}<br />
                     </Fragment>
                 ))}
             </TableCell>
