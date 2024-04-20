93fbdffb4d4945ddf9c67587b9088681f6116625
Takkun0310 <takkunsan0310@gmail.com>
2024-04-21 02:41:04 +0900

    チーム情報の誤り注意用Alertを追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,29 +4,34 @@ import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import { userinfoFactory } from "@/src/models/UserinfoModel";
 import ProfileCard from "@/components/ProfileCard";
-import {WarningRounded} from "@mui/icons-material";
+import { WarningRounded } from "@mui/icons-material";
 
-export default async function  Userinfo() {
+export default async function Userinfo() {
     const userInfo = await userinfoFactory().fetch();
+
+    const showTeamError = userInfo && userInfo.teamIds.length > 2;
 
     return (
         <>
-            <AppBar position="fixed"
-                    sx={{
-                        color: 's-lightest.main',
-                        background: '#6C75AB',
-                        height: '70px'
-                    }}
+            <AppBar
+                position="fixed"
+                sx={{
+                    color: 's-lightest.main',
+                    background: '#6C75AB',
+                    height: '70px'
+                }}
             />
-            <Box padding={2}
-                 sx={{
-                     height: '100vh',
-                     display: 'flex',
-                     flexDirection: 'column',
-                     alignItems: 'center',
-                     justifyContent: 'center',
-                     marginTop: '70px',
-                 }}>
+            <Box
+                padding={2}
+                sx={{
+                    height: '100vh',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginTop: '30px',
+                }}
+            >
 
                 <Stack
                     spacing={4}
@@ -34,46 +39,85 @@ export default async function  Userinfo() {
                     alignItems='stretch'
                     justifyContent='center'
                 >
-                    <Stack spacing={1}>
-                        <Typography
-                            variant="h5"
-                            color="s-darker.main"
-                            fontWeight="bold"
-                            sx={{
-                                textAlign: 'center',
-                                width: '100%'
-                            }}
-                        >
-                            {userInfo.name ?? "unknown"} さん
-                        </Typography>
-                        <Typography variant="h5"
-                                    color="s-darker.main"
-                                    fontWeight="bold"
+                    <Stack spacing={5} sx={{ position: 'relative' }}>
+                        <Stack spacing={1}>
+                            <Typography
+                                variant="h5"
+                                color="s-darker.main"
+                                fontWeight="bold"
+                                sx={{
+                                    textAlign: 'center',
+                                    width: '100%'
+                                }}
+                            >
+                                {userInfo.name ?? "unknown"} さん
+                            </Typography>
+                            <Typography variant="h5"
+                                        color="s-darker.main"
+                                        fontWeight="bold"
+                                        sx={{
+                                            textAlign: 'center',
+                                            width: '100%'
+                                        }}>
+                                あなたの情報を確認しましょう
+                            </Typography>
+                        </Stack>
+                        {!showTeamError && userInfo && <ProfileCard user={userInfo} />}
+                        <Stack spacing={2}>
+                            <Alert
+                                variant="outlined"
+                                icon={false}
+                                sx={{
+                                    width: '100%',
+                                    maxWidth: '400px',
+                                    fontWeight: 'bold',
+                                    color: '#A22E1D',
+                                    borderRadius: '9px',
+                                    backgroundColor: '#FDE3DE',
+                                    borderColor: '#A22E1D',
+                                    py: 1.5,
+                                    display: showTeamError ? 'none' : 'block',
+                                }}
+                            >
+                                <Box display="flex" alignItems="center">
+                                    <WarningRounded fontSize="medium" />
+                                    <Box sx={{ paddingLeft: '8px', display: 'flex', alignItems: 'center' }}>
+                                        <Typography variant="subtitle2" fontWeight='bold' sx={{ lineHeight: 1 }}>
+                                            間違っていたら体育局員に必ず知らせてください！
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            </Alert>
+
+
+
+                            {showTeamError && (
+                                <Alert
+                                    variant="outlined"
+                                    icon={false}
                                     sx={{
-                                        textAlign: 'center',
-                                        width: '100%'
-                                    }}>
-                            あなたの情報を確認しましょう
-                        </Typography>
+                                        width: '100%',
+                                        maxWidth: '400px',
+                                        fontWeight: 'bold',
+                                        color: '#A22E1D',
+                                        borderRadius: '9px',
+                                        backgroundColor: '#FDE3DE',
+                                        borderColor: '#A22E1D',
+                                        py: 1.5,
+                                    }}
+                                >
+                                    <Box display="flex" alignItems="center">
+                                        <WarningRounded fontSize="medium" />
+                                        <Box sx={{ paddingLeft: '8px', display: 'flex', alignItems: 'center' }}>
+                                            <Typography variant="subtitle2" fontWeight='bold' sx={{ lineHeight: 1 }}>
+                                                あなたは３つ以上のチームに所属しています！
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                </Alert>
+                            )}
+                        </Stack>
                     </Stack>
-
-
-                    {userInfo && <ProfileCard user={userInfo}/>}
-                    <Alert
-                        variant="outlined"
-                        severity="error"
-                        iconMapping={{ error: <WarningRounded fontSize="inherit" /> }}  // アイコンを指定
-                        sx={{
-                            fontWeight: 'bold',
-                            color: '#A22E1D',
-                            borderRadius: '9px',
-                            backgroundColor: '#FDE3DE',
-                            borderColor: '#A22E1D',
-                            py: 1.5
-                        }}
-                    >
-                        間違っていたら体育局員に必ず知らせてください！
-                    </Alert>
                 </Stack>
             </Box>
         </>
