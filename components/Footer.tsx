998240c3ff1273542f3e533fec3ea79ec9d87a34
Takkun0310 <takkunsan0310@gmail.com>
2024-04-23 18:13:53 +0900

    FooterのButtonの表示設定

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,9 @@
-import {Button, Stack, Typography, Paper, BottomNavigation} from "@mui/material";
+import { Stack, Typography, Paper, BottomNavigation} from "@mui/material";
+import { ConfirmButton } from "@/components/ConfirmButton";
+import { BackButton } from "@/components/BackButton";
 
 const Footer = () => {
+
     return (
         <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0, }}>
             <BottomNavigation
@@ -19,32 +22,8 @@ const Footer = () => {
                     <Typography variant='subtitle1' component='div'>
                         チーム登録フロー：晴天時のチーム登録→雨天時のチーム登録→登録漏れの確認→完了
                     </Typography>
-                    <Button
-                        variant='contained'
-                        href={"/form"}
-                        disableElevation={true}
-                        sx={{
-                            flex: '0.18',
-                            py: 1.0,
-                            color: 's-lightest.main',
-                            backgroundColor: 's-lighter.main',
-                            borderRadius: "10px",
-                        }}>
-                        戻る
-                    </Button>
-                    <Button
-                        variant='contained'
-                        href={"/list"}
-                        disableElevation={true}
-                        sx={{
-                            flex: '0.18',
-                            py: 1.0,
-                            backgroundColor: 's-light.main',
-                            borderRadius: "10px",
-                            color: 's-dark.main'
-                        }}>
-                        確認画面に進む
-                    </Button>
+                    <BackButton />
+                    <ConfirmButton />
                 </Stack>
             </BottomNavigation>
         </Paper>
